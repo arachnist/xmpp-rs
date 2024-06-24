c94c53ac8611eece4fccf740c6d7f096e6a512ad
mb <mb@nesium.com>
2024-06-24 16:00:47 +0200

    Reorganize imports in data_forms module

--- parsers/src/data_forms/mod.rs
+++ parsers/src/data_forms/mod.rs
@@ -4,10 +4,10 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-pub use data_form::*;
-pub use validate::*;
-
 /// XEP-0004: Data Forms
 pub mod data_form;
 
 pub mod validate;
+
+pub use data_form::*;
+pub use validate::*;
