arachnist/xmpp-rs

commit d4d520e1f65f943d3a7a3e5ddb564c0c9b3442f4

xso-proc: add support for built-in prefixes in attribute names
This simplifies the use of built-in XML attributes such as xml:lang.

xso-proc/src/meta.rs
@@ -15,6 +15,11 @@ use syn::{meta::ParseNestedMeta, spanned::Spanned, *};
 
 use rxml_validation::NcName;
 
+/// XML core namespace URI (for the `xml:` prefix)
+pub const XMLNS_XML: &'static str = "http://www.w3.org/XML/1998/namespace";
+/// XML namespace URI (for the `xmlns:` prefix)
+pub const XMLNS_XMLNS: &'static str = "http://www.w3.org/2000/xmlns/";
+
 /// Value for the `#[xml(namespace = ..)]` attribute.
 #[derive(Debug)]
 pub(crate) enum NamespaceRef {
@@ -23,6 +28,12 @@ pub(crate) enum NamespaceRef {
 
     /// The XML namespace is specified as a path.
     Path(Path),
+}
+
+impl NamespaceRef {
+    fn fudge(value: &str, span: Span) -> Self {
+        Self::LitStr(LitStr::new(value, span))
+    }
 }
 
 impl syn::parse::Parse for NamespaceRef {
@@ -67,10 +78,7 @@ impl syn::parse::Parse for NameRef {
             let span = s.span();
             match NcName::try_from(s.value()) {
                 Ok(value) => Ok(Self::Literal { value, span }),
-                Err(e) => Err(Error::new(
-                    span,
-                    format!("not a valid XML element name: {}", e),
-                )),
+                Err(e) => Err(Error::new(span, format!("not a valid XML name: {}", e))),
             }
         } else {
             let p: Path = input.parse()?;
@@ -195,6 +203,44 @@ impl XmlCompoundMeta {
     }
 }
 
+/// Parse an XML name while resolving built-in namespace prefixes.
+fn parse_prefixed_name(
+    value: syn::parse::ParseStream<'_>,
+) -> Result<(Option<NamespaceRef>, NameRef)> {
+    let name: LitStr = value.parse()?;
+    let name_span = name.span();
+    let (prefix, name) = match name
+        .value()
+        .try_into()
+        .and_then(|name: rxml_validation::Name| name.split_name())
+    {
+        Ok(v) => v,
+        Err(e) => {
+            return Err(Error::new(
+                name_span,
+                format!("not a valid XML name: {}", e),
+            ))
+        }
+    };
+    let name = NameRef::Literal {
+        value: name,
+        span: name_span,
+    };
+    if let Some(prefix) = prefix {
+        let namespace_uri = match prefix.as_str() {
+            "xml" => XMLNS_XML,
+            "xmlns" => XMLNS_XMLNS,
+            other => return Err(Error::new(
+                name_span,
+                format!("prefix `{}` is not a built-in prefix and cannot be used. specify the desired namespace using the `namespace` key instead.", other)
+            )),
+        };
+        Ok((Some(NamespaceRef::fudge(namespace_uri, name_span)), name))
+    } else {
+        Ok((None, name))
+    }
+}
+
 /// Contents of an `#[xml(..)]` attribute on a struct or enum variant member.
 #[derive(Debug)]
 pub(crate) enum XmlFieldMeta {
@@ -222,10 +268,11 @@ impl XmlFieldMeta {
     fn attribute_from_meta(meta: ParseNestedMeta<'_>) -> Result<Self> {
         if meta.input.peek(Token![=]) {
             // shorthand syntax
+            let (namespace, name) = parse_prefixed_name(meta.value()?)?;
             Ok(Self::Attribute {
                 span: meta.path.span(),
-                name: Some(meta.value()?.parse()?),
-                namespace: None,
+                name: Some(name),
+                namespace,
             })
         } else if meta.input.peek(syn::token::Paren) {
             // full syntax
@@ -236,11 +283,31 @@ impl XmlFieldMeta {
                     if name.is_some() {
                         return Err(Error::new_spanned(meta.path, "duplicate `name` key"));
                     }
-                    name = Some(meta.value()?.parse()?);
+                    let value = meta.value()?;
+                    name = if value.peek(LitStr) {
+                        let name_span = value.span();
+                        let (new_namespace, name) = parse_prefixed_name(value)?;
+                        if let Some(new_namespace) = new_namespace {
+                            if namespace.is_some() {
+                                return Err(Error::new(
+                                    name_span,
+                                    "cannot combine `namespace` key with prefixed `name`",
+                                ));
+                            }
+                            namespace = Some(new_namespace);
+                        }
+                        Some(name)
+                    } else {
+                        // just use the normal parser
+                        Some(value.parse()?)
+                    };
                     Ok(())
                 } else if meta.path.is_ident("namespace") {
                     if namespace.is_some() {
-                        return Err(Error::new_spanned(meta.path, "duplicate `namespace` key"));
+                        return Err(Error::new_spanned(
+                            meta.path,
+                            "duplicate `namespace` key or `name` key has prefix",
+                        ));
                     }
                     namespace = Some(meta.value()?.parse()?);
                     Ok(())

parsers/src/util/macro_tests.rs
@@ -282,3 +282,20 @@ fn namespaced_attribute_roundtrip_b() {
           xmlns:tns1='urn:example:ns2' tns1:foo='a2'/>",
     );
 }
+
+#[derive(FromXml, IntoXml, PartialEq, Debug, Clone)]
+#[xml(namespace = NS1, name = "attr")]
+struct PrefixedAttribute {
+    #[xml(attribute = "xml:lang")]
+    lang: String,
+}
+
+#[test]
+fn prefixed_attribute_roundtrip() {
+    #[allow(unused_imports)]
+    use std::{
+        option::Option::{None, Some},
+        result::Result::{Err, Ok},
+    };
+    roundtrip_full::<PrefixedAttribute>("<attr xmlns='urn:example:ns1' xml:lang='foo'/>");
+}

commit c0d109d9be905cbc42cbef2dde3722daba3b494d

xso-proc: fix renaming attributes using a static item
This was broken by d4d520e by accident, and of course we didn't have
tests for that :-).

(we do, now)

xso-proc/src/meta.rs
@@ -207,6 +207,12 @@ impl XmlCompoundMeta {
 fn parse_prefixed_name(
     value: syn::parse::ParseStream<'_>,
 ) -> Result<(Option<NamespaceRef>, NameRef)> {
+    if !value.peek(LitStr) {
+        // if we don't have a string literal next, we delegate to the default
+        // `NameRef` parser.
+        return Ok((None, value.parse()?));
+    }
+
     let name: LitStr = value.parse()?;
     let name_span = name.span();
     let (prefix, name) = match name
@@ -284,23 +290,18 @@ impl XmlFieldMeta {
                         return Err(Error::new_spanned(meta.path, "duplicate `name` key"));
                     }
                     let value = meta.value()?;
-                    name = if value.peek(LitStr) {
-                        let name_span = value.span();
-                        let (new_namespace, name) = parse_prefixed_name(value)?;
-                        if let Some(new_namespace) = new_namespace {
-                            if namespace.is_some() {
-                                return Err(Error::new(
-                                    name_span,
-                                    "cannot combine `namespace` key with prefixed `name`",
-                                ));
-                            }
-                            namespace = Some(new_namespace);
+                    let name_span = value.span();
+                    let (new_namespace, new_name) = parse_prefixed_name(value)?;
+                    if let Some(new_namespace) = new_namespace {
+                        if namespace.is_some() {
+                            return Err(Error::new(
+                                name_span,
+                                "cannot combine `namespace` key with prefixed `name`",
+                            ));
                         }
-                        Some(name)
-                    } else {
-                        // just use the normal parser
-                        Some(value.parse()?)
-                    };
+                        namespace = Some(new_namespace);
+                    }
+                    name = Some(new_name);
                     Ok(())
                 } else if meta.path.is_ident("namespace") {
                     if namespace.is_some() {

parsers/src/util/macro_tests.rs
@@ -66,6 +66,13 @@ type Result = ((),);
 
 static NS1: &str = "urn:example:ns1";
 
+static SOME_NAME: &::xso::exports::rxml::strings::NcNameStr = {
+    #[allow(unsafe_code)]
+    unsafe {
+        ::xso::exports::rxml::strings::NcNameStr::from_str_unchecked("bar")
+    }
+};
+
 #[derive(FromXml, IntoXml, PartialEq, Debug, Clone)]
 #[xml(namespace = NS1, name = "foo")]
 struct Empty;
@@ -149,13 +156,6 @@ fn empty_qname_check_has_precedence_over_attr_check() {
     }
 }
 
-static SOME_NAME: &::xso::exports::rxml::strings::NcNameStr = {
-    #[allow(unsafe_code)]
-    unsafe {
-        ::xso::exports::rxml::strings::NcNameStr::from_str_unchecked("bar")
-    }
-};
-
 #[derive(FromXml, IntoXml, PartialEq, Debug, Clone)]
 #[xml(namespace = NS1, name = SOME_NAME)]
 struct NamePath;
@@ -234,6 +234,8 @@ fn required_attribute_missing() {
 struct RenamedAttribute {
     #[xml(attribute = "a1")]
     foo: String,
+    #[xml(attribute = SOME_NAME)]
+    bar: String,
 }
 
 #[test]
@@ -243,7 +245,7 @@ fn renamed_attribute_roundtrip() {
         option::Option::{None, Some},
         result::Result::{Err, Ok},
     };
-    roundtrip_full::<RenamedAttribute>("<attr xmlns='urn:example:ns1' a1='bar'/>");
+    roundtrip_full::<RenamedAttribute>("<attr xmlns='urn:example:ns1' a1='bar' bar='baz'/>");
 }
 
 #[derive(FromXml, IntoXml, PartialEq, Debug, Clone)]